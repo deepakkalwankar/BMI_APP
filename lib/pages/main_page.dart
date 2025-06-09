35266f29b28a4ecd4b7dc32c0dd324264583ae41
deepakkalwankar <deepakkalvankar13@gmail.com>
2025-06-09 20:09:37 +0530

    inital commit

---
+++ lib/pages/main_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/cupertino.dart';
+
+import 'bmi_page.dart';
+import 'history_page.dart';
+// import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MainPageState();
+  }
+}
+
+class _MainPageState extends State<MainPage> {
+  final List<Widget> _tabs = [
+    BMIPage(),
+    HistoryPage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+      navigationBar: const CupertinoNavigationBar(
+        middle: Text('IBMI'),
+      ),
+      child: CupertinoTabScaffold(
+        tabBar: CupertinoTabBar(
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.home),
+              label: "BMI",
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(CupertinoIcons.person),
+              label: "History",
+            ),
+          ],
+        ),
+        tabBuilder: (context, index) {
+          return CupertinoTabView(
+            builder: (context) {
+              return _tabs[index];
+            },
+          );
+        },
+      ),
+    );
+  }
+}
